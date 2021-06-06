843a9432f6efde9a8f9b89c6903d78e6be5df599
yuralol <84666118+yuralol@users.noreply.github.com>
2021-06-06 12:14:25 +0300

    создали вертикальную палочку Stick

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -9,10 +9,10 @@ namespace Tetris
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            Square s = new Square(2, 5, '&');
+            Square s = new Square(2, 5, '*');
             s.Draw();
 
-            Point p1 = new Point(2, 3, '0');
+            Point p1 = new Point(2, 3, '*');
             p1.Draw();
 
             Point p2 = new Point()
---
+++ Tetris/Tetris/Stick.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Stick
+    {
+        Point[] points = new Point[4];
+
+        public Stick(int x, int y, char sym)
+        {
+            points[0] = new Point(x, y, sym);
+            points[1] = new Point(x, y + 1, sym);
+            points[2] = new Point(x, y + 2, sym);
+            points[3] = new Point(x, y + 3, sym);
+        }
+        public void Draw()
+        {
+            foreach (Point p in points)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
