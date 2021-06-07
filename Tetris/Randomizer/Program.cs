4a66adc035612ea82f413a77a3a01e99b5af169e
yuralol <84666118+yuralol@users.noreply.github.com>
2021-06-07 12:55:24 +0300

    рандом

---
+++ Tetris/Randomizer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Randomizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            
+
+            for(int i = 0; i < 10; i++)
+            {
+                int value = rand.Next(0, 100);
+                Console.WriteLine(value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
