c43a4ebb2ca9b848808224467daef8e901045751
yuralol <84666118+yuralol@users.noreply.github.com>
2021-06-07 11:41:52 +0300

    Создали Enum

---
+++ Tetris/Enums/Day.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    enum Day
+    {
+        Mon = 1, Tue, Wed, Thue, Fri, Sat, Sun
+    }
+}
---
+++ Tetris/Enums/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day day = Day.Mon;
+            //int i = 3;
+
+            //Day day3 = (Day)i;
+            //Console.WriteLine(day3);
+
+
+            if (day == Day.Mon)
+            {
+                Console.WriteLine("Monday");
+                Console.WriteLine((int)day);
+            }
+        
+            Console.ReadLine();
+        }
+    }
+}
