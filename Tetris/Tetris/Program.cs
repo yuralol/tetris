8721ec93b1f4bd4712f504b48ce53fd3b2db5818
yuralol <84666118+yuralol@users.noreply.github.com>
2021-06-07 13:22:44 +0300

    вывод рандомных фигур вниз

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -11,56 +11,27 @@ namespace Tetris
             Console.SetBufferSize(40, 30);
 
             FigureGenerator generator = new FigureGenerator(20, 0, '*');
-            Figure s = generator.GetNewFigure();
+            Figure s = null;
 
-            s.Draw();
-            Thread.Sleep(1000);
-
-            s.Hide();
-            s.Rotate();
-            s.Draw();
-
-            Thread.Sleep(1000);
-            s.Hide();
-            s.Move(Direction.LEFT);
-            s.Draw();
-
-            Thread.Sleep(1000);
-            s.Hide();
-            s.Move(Direction.DOWN);
-            s.Draw();
-
-            Thread.Sleep(1000);
-            s.Hide();
-            s.Move(Direction.DOWN);
-            s.Draw();
-
-            s.Hide();
-            s.Rotate();
-            s.Draw();
-
-            Thread.Sleep(1000);
-            s.Hide();
-            s.Move(Direction.RIGHT);
-            s.Draw();
-
-            //Stick stick = new Stick(6, 6, '*');
-            //stick.Draw();
-
-            //Point p1 = new Point(2, 3, '*');
-            //p1.Draw();
-
-            Point p2 = new Point()
+            while(true)
             {
-                x = 4,
-                y = 5,
-            };
-
-            p2.Draw();
-
-
-            Console.ReadLine();
+                FigureFall(s, generator);
+                s.Draw();
+            }
         }
 
+        static void FigureFall(Figure fig, FigureGenerator generator)
+        {
+            fig = generator.GetNewFigure();
+            fig.Draw();
+
+            for (int i = 0; i < 15; i++)
+            {
+                fig.Hide();
+                fig.Move(Direction.DOWN);
+                fig.Draw();
+                Thread.Sleep(200);
+            }
+        }
     }
 }
